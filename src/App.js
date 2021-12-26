1eccd9a903693c2fc7dd1632e978f9e768f312fe
Truong Tuan Anh <anhttfx13476@funix.edu.vn>
2021-12-26 21:26:03 +0900

    Lab03_2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from "./shared/dishes";
 
 class App extends Component {
 
+  constructor(prop){
+    super(prop);
+    this.state = {
+      dishes : DISHES,
+    }
+  }
   render() {
     return (
       <div>
@@ -15,7 +22,7 @@ class App extends Component {
         </NavbarBrand>
       </div>
     </Navbar>
-    <Menu/>
+    <Menu dishes = {this.state.dishes}/>
       </div>
     );
   }
