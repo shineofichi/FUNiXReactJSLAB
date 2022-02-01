8a03baaa2cc2522adee5f6ed864e07ba043df932
Truong Tuan Anh <anhttfx13476@funix.edu.vn>
2022-02-01 21:17:07 +0900

    Lab11-2

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,8 +8,9 @@ import {
   Card,
 } from "reactstrap";
 import { baseUrl } from "../shared/baseUrl";
-
+import { FadeTransform } from "react-animation-components";
 import { Loading } from "./LoadingComponent";
+
 function Home(props) {
   return (
     <div className="container">
@@ -55,16 +56,23 @@ function RenderCard({ item, isLoading, errMess }) {
     );
   } else console.log(item);
   return (
-    <Card>
-      <CardImg src={baseUrl + item.image} alt={item.name} />
-      <CardBody>
-        <CardTitle>{item.name}</CardTitle>
-        {item.designation ? (
-          <CardSubtitle>{item.designation}</CardSubtitle>
-        ) : null}
-        <CardText>{item.description}</CardText>
-      </CardBody>
-    </Card>
+    <FadeTransform
+      in
+      transformProps={{
+        exitTransform: "scale(0.5) translateY(-50%)",
+      }}
+    >
+      <Card>
+        <CardImg src={baseUrl + item.image} alt={item.name} />
+        <CardBody>
+          <CardTitle>{item.name}</CardTitle>
+          {item.designation ? (
+            <CardSubtitle>{item.designation}</CardSubtitle>
+          ) : null}
+          <CardText>{item.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
