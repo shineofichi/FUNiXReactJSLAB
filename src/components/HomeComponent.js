3e65df047afc8621db55fcfa05f6202f6e7f3593
Truong Tuan Anh <anhttfx13476@funix.edu.vn>
2022-02-07 21:37:50 +0900

    Lab05-3

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,44 @@
 import React from "react";
+import {
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
+  CardSubtitle,
+  Card,
+} from "reactstrap";
 
-function Home( prop){
-    return(
-        <div className="container">
-            Home
+function Home(props) {
+  return (
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} />
         </div>
-    )
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function RenderCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default Home;
